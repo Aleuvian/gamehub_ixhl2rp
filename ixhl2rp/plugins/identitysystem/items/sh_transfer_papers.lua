3fb53ed73292a939ab70d5a868efd06496a3adf2
Wachter <34869193+Upas-Source@users.noreply.github.com>
2022-09-10 02:14:53 -0500

    Update sh_transfer_papers.lua
    removed function get description, unused

--- ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
+++ ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
@@ -2,15 +2,12 @@ ITEM.name = "Transfer Documents"
 ITEM.model = Model("models/zadrakos/combinepassports/passport.mdl")
 ITEM.description = "A small booklet detailing your transfer status."
 
-function ITEM:GetDescription()
-	return self.description
-end
 function ITEM:PopulateTooltip(tooltip)
 	local data = tooltip:AddRow("data")
 	data:SetBackgroundColor(derma.GetColor("Success", tooltip))
 	data:SetText("Name: " .. self:GetData("citizen_name", character:GetName()) .. "\nTransfer Number: " .. self:GetData("cid", character:GetData("cid", id)) .. "\nIssue Date: " .. self:GetData("issue_date", "Unissued")) -- Here's hoping this work, how do I do dates?
 	data:SetFont("BudgetLabel")
-	data:SetExpensiveShadow(0.5 )
+	data:SetExpensiveShadow(0.5)
 	data:SizeToContents()
 
 	local data2 = tooltip:AddRow("data2")
@@ -19,7 +16,4 @@ function ITEM:PopulateTooltip(tooltip)
 	data2:SetFont("DermaDefault")
 	data2:SetExpensiveShadow(0.5)
 	data2:SizeToContents()
-
-
-
 end
