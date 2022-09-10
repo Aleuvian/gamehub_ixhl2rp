513893acb1bf75661e2594a682d5026ecb06df87
Wachter <34869193+Upas-Source@users.noreply.github.com>
2022-09-10 02:30:50 -0500

    Update sh_transfer_papers.lua

--- ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
+++ ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
@@ -3,21 +3,22 @@ ITEM.model = Model("models/zadrakos/combinepassports/passport.mdl")
 ITEM.description = "A small booklet detailing your transfer status."
 
 function ITEM:PopulateTooltip(tooltip)
+	
 	local line1 = tooltip:AddRow("line1")
-  local line2 = tooltip:AddRow("line2")
-  local line3 = tooltip:AddRow("line3")
 	line1:SetBackgroundColor(derma.GetColor("Success", tooltip))
 	line1:SetText("Name: " .. self:GetData("citizen_name"))
 	line1:SetFont("BudgetLabel")
 	line1:SetExpensiveShadow(0.5)
 	line1:SizeToContents()
 	
+	local line2 = tooltip:AddRow("line2")
 	line2:SetBackgroundColor(derma.GetColor("Success", tooltip))
   	line2:SetText("\nTransfer Number: " .. self:GetData("cid"))
 	line2:SetFont("BudgetLabel")
 	line2:SetExpensiveShadow(0.5)
 	line2:SizeToContents()
 	
+	local line3 = tooltip:AddRow("line3")
 	line3:SetBackgroundColor(derma.GetColor("Success", tooltip))
   	line3:SetText("\nIssue Date " .. self:GetData("issue_date", "Unissued"))
 	line3:SetFont("BudgetLabel")
