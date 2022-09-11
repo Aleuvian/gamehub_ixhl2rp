f6a6df98256e928e09f3794ba8a2876dde625162
Wachter <34869193+Upas-Source@users.noreply.github.com>
2022-09-11 12:37:37 -0500

    Update sh_transfer_papers.lua

--- ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
+++ ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
@@ -5,20 +5,23 @@ ITEM.description = "A small booklet detailing your transfer status."
 function ITEM:GetDescription()
 	return self.description
 end
-	
-function ITEM:PopulateTooltip(tooltip)
-    
-    local data = tooltip:AddRow("data")
-    data:SetBackgroundColor(derma.GetColor("Success", tooltip))
-    data:SetText("Name: " .. self:GetOwner() .. "\nID Number: " .. self:GetID())
-    data:SetFont("BudgetLabel")
-    data:SetExpensiveShadow(0.5)
-    data:SizeToContents()
 
-    local data2 = tooltip:AddRow("data2")
-    data2:SetBackgroundColor(derma.GetColor("Warning", tooltip))
-    data2:SetText("Take these papers to a CCA officer, to receive a CID in return. THIS IS NOT VALID IDENTIFICATION.")
-    data2:SetFont("DermaDefault")
-    data2:SetExpensiveShadow(0.5)
-    data2:SizeToContents()
+function PLUGIN:OnCharacterCreated(client, character)
+
+	function ITEM:PopulateTooltip(tooltip)
+		
+    		local data = tooltip:AddRow("data")
+    		data:SetBackgroundColor(derma.GetColor("Success", tooltip))
+    		data:SetText("Name: " .. character:GetName() .. "\nID Number: " .. character:GetData("Cid", id)
+    		data:SetFont("BudgetLabel")
+    		data:SetExpensiveShadow(0.5)
+    		data:SizeToContents()
+
+    		local data2 = tooltip:AddRow("data2")
+    		data2:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+    		data2:SetText("Take these papers to a CCA officer, to receive a CID in return. THIS IS NOT VALID IDENTIFICATION.")
+    		data2:SetFont("DermaDefault")
+    		data2:SetExpensiveShadow(0.5)
+    		data2:SizeToContents()
+	end
 end
