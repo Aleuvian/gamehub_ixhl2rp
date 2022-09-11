c9bd701903f80762d15d0df369069023e0a442c3
Aleuvian <18jburnett@gmail.com>
2022-09-11 15:48:19 -0400

    Update cl_hooks.lua

--- ixhl2rp/plugins/cto/cl_hooks.lua
+++ ixhl2rp/plugins/cto/cl_hooks.lua
@@ -275,14 +275,14 @@ function PLUGIN:HUDPaint()
 					local showDetail = (Vector(toScreen.x, toScreen.y):Distance(halfScrVector) <= lowDetailBox)
 					local CID = v:GetCharacter():GetData("cid", "UNKNOWN")
 					
-					if (ix.config.Get("useTagSystem") and beholderEyePos:Distance(v:GetPos()) <= (maximumDistance / 6) and !v:GetCharacter():GetData("IsCIDTagGone") and CID != "") and Inventory:HasItem("cid") then
+					if (ix.config.Get("useTagSystem") and beholderEyePos:Distance(v:GetPos()) <= (maximumDistance / 6) and !v:GetCharacter():GetData("IsCIDTagGone") and CID != "" and !v:GetCharacter():GetInventory("cid_new") ) then
 						local text = "<:: c#" .. CID .. " ::>"
 						local color = team.GetColor(v:Team()) or color_white
 
 						draw.SimpleText(showDetail and text or lowDetailText, "BudgetLabel", toScreen.x, toScreen.y, color, 1, 1)
 						toScreen.y = toScreen.y + fontHeight
 					else
-						local text = "<:: !RFID MISSING! ::>"
+						local text = "<:: RFID MISSING ::>"
 						local color = team.GetColor (v:Team()) or color_white
 
 						draw.SimpleText(showDetail and text or lowDetailText, "BudgetLabel", toScreen.x, toScreen.y, color, 1, 1)
