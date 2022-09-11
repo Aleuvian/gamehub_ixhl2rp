6141a5bfa545ed263fbc2dbdb96c48a972e934e9
Wachter <34869193+Upas-Source@users.noreply.github.com>
2022-09-11 13:18:49 -0500

    Update sh_transfer_papers.lua

--- ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
+++ ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
@@ -8,24 +8,23 @@ end
 
 function ITEM:PopulateTooltip(tooltip) 
 
-	self:Hook("OnCharacterCreated", function(client, character)
+	self:Hook("OnCharacterCreated", function(client, character))
 		
-    		local data = tooltip:AddRow("data")
-    		data:SetBackgroundColor(derma.GetColor("Success", tooltip))
-    		data:SetText("Name: " .. character:GetName() .. "\nID Number: " .. character:GetData("cid", id))
-    		data:SetFont("BudgetLabel")
-    		data:SetExpensiveShadow(0.5)
-    		data:SizeToContents()
-		print(character:GetName(), character:GetData("cid", id), "Print is a Success!")
-		print("If nothing printed above this, FAIL.")
+    	local data = tooltip:AddRow("data")
+    	data:SetBackgroundColor(derma.GetColor("Success", tooltip))
+    	data:SetText("Name: " .. character:GetName() .. "\nID Number: " .. character:GetData("cid", id))
+    	data:SetFont("BudgetLabel")
+    	data:SetExpensiveShadow(0.5)
+    	data:SizeToContents()
+	print(character:GetName(), character:GetData("cid", id), "Print is a Success!")
+	print("If nothing printed above this, FAIL.")
 		
 		
 		
-    		local data2 = tooltip:AddRow("data2")
-    		data2:SetBackgroundColor(derma.GetColor("Warning", tooltip))
-    		data2:SetText("Take these papers to a CCA officer, to receive a CID in return. THIS IS NOT VALID IDENTIFICATION.")
-    		data2:SetFont("DermaDefault")
-    		data2:SetExpensiveShadow(0.5)
-    		data2:SizeToContents()
-	end)
+    	local data2 = tooltip:AddRow("data2")
+    	data2:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+    	data2:SetText("Take these papers to a CCA officer, to receive a CID in return. THIS IS NOT VALID IDENTIFICATION.")
+    	data2:SetFont("DermaDefault")
+    	data2:SetExpensiveShadow(0.5)
+    	data2:SizeToContents()
 end
