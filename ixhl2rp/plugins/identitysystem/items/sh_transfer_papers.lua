b6ba2fb787c1f5b1d806d2e13342db0f9b897661
Wachter <34869193+Upas-Source@users.noreply.github.com>
2022-09-11 08:57:39 -0500

    Update sh_transfer_papers.lua

--- ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
+++ ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
@@ -2,33 +2,38 @@ ITEM.name = "Transfer Documents"
 ITEM.model = Model("models/zadrakos/combinepassports/passport.mdl")
 ITEM.description = "A small booklet detailing your transfer status."
 
+function ITEM:GetDescription()
+	return self.description
+end
+
+
 function ITEM:PopulateTooltip(tooltip)
     
-    local line1 = tooltip:AddRow("line1")
-    line1:SetBackgroundColor(derma.GetColor("Success", tooltip))
-    line1:SetText("Name: "..client:player:GetName())
-    line1:SetFont("BudgetLabel")
-    line1:SetExpensiveShadow(0.5)
-    line1:SizeToContents()
+    local data = tooltip:AddRow("data")
+    data:SetBackgroundColor(derma.GetColor("Success", tooltip))
+    data:SetText("Name: "..client:player:GetName(" "))
+    data:SetFont("BudgetLabel")
+    data:SetExpensiveShadow(0.5)
+    data:SizeToContents()
     
-    local line2 = tooltip:AddRow("line2")
-    line2:SetBackgroundColor(derma.GetColor("Success", tooltip))
-    line2:SetText("Transfer Number: "..client:character:GetID())
-    line2:SetFont("BudgetLabel")
-    line2:SetExpensiveShadow(0.5)
-    line2:SizeToContents()
+    local data = tooltip:AddRow("data")
+    data:SetBackgroundColor(derma.GetColor("Success", tooltip))
+    data:SetText("Transfer Number: "..client:character:GetID(" "))
+    data:SetFont("BudgetLabel")
+    data:SetExpensiveShadow(0.5)
+    data:SizeToContents()
     
-    local line3 = tooltip:AddRow("line3")
-    line3:SetBackgroundColor(derma.GetColor("Success", tooltip))
-    line3:SetText("Issue date: "..ix.date.Get())
-    line3:SetFont("BudgetLabel")
-    line3:SetExpensiveShadow(0.5)
-    line3:SizeToContents()
+    local data = tooltip:AddRow("line3")
+    data:SetBackgroundColor(derma.GetColor("Success", tooltip))
+    data:SetText("Issue date: "..ix.date.Get(" "))
+    data:SetFont("BudgetLabel")
+    data:SetExpensiveShadow(0.5)
+    data:SizeToContents()
 
-    local line4 = tooltip:AddRow("line4")
-    line4:SetBackgroundColor(derma.GetColor("Warning", tooltip))
-    line4:SetText("Take these papers to a CCA officer, to receive a CID in return. THIS IS NOT VALID IDENTIFICATION.")
-    line4:SetFont("DermaDefault")
-    line4:SetExpensiveShadow(0.5)
-    line4:SizeToContents()
+    local data = tooltip:AddRow("data")
+    data:SetBackgroundColor(derma.GetColor("Warning", tooltip))
+    data:SetText("Take these papers to a CCA officer, to receive a CID in return. THIS IS NOT VALID IDENTIFICATION.")
+    data:SetFont("DermaDefault")
+    data:SetExpensiveShadow(0.5)
+    data:SizeToContents()
 end
