3e3ba96735c584c5bf5f29388540a1e0bbfa4aac
Wachter <34869193+Upas-Source@users.noreply.github.com>
2022-09-11 10:06:26 -0500

    Update sh_transfer_papers.lua

--- ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
+++ ixhl2rp/plugins/identitysystem/items/sh_transfer_papers.lua
@@ -12,7 +12,7 @@ function ITEM:PopulateTooltip(tooltip)
     
     local data = tooltip:AddRow("data")
     data:SetBackgroundColor(derma.GetColor("Success", tooltip))
-    data:SetText("Name: " .. self:GetOwner()) .. --"\nID Number: " .. self:GetData("cid")
+    data:SetText("Name: " .. self:GetOwner()) --"\nID Number: " .. self:GetData("cid")
     data:SetFont("BudgetLabel")
     data:SetExpensiveShadow(0.5)
     data:SizeToContents()
